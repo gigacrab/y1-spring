df655abb30ff6b2cc0b7a1319ee99dba7cb9b808
gigacrab <tehwenliang@gmail.com>
2026-03-02 11:33:09 +0800

    new commit

--- camera.py
+++ camera.py
@@ -3,7 +3,7 @@ from picamera2 import Picamera2
 import time
 
 picam2 = Picamera2()
-camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)})
+camera_config = picam2.create_still_configuration(main={"size": (1280, 720)})
 picam2.configure(camera_config)
 picam2.start()
 time.sleep(2)

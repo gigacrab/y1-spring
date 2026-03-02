306af933763ad2810a8b601bb2fe1a8951ec82e3
gigacrab <tehwenliang@gmail.com>
2026-03-02 11:44:15 +0800

    new commit

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 from picamera2 import Picamera2
 import time
+import numpy as np
 
 picam2 = Picamera2()
 camera_config = picam2.create_still_configuration(main={"size": (1280, 720)})
@@ -13,7 +14,9 @@ im = picam2.capture_array()
 #cv2.imshow("output", img)
 imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(imgray, 127, 255, 0)
-im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+im2 = np.zeros((1280, 720, 3), dtype=np.uint8)
+cv.drawContours(im2, contours, -1, (255, 255, 255), 1)
 cv.imshow(im2)
 cv.waitKey(0)
 

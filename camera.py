d29883734522726ea4012af6a4f07ae87384a42a
gigacrab <tehwenliang@gmail.com>
2026-03-02 10:49:01 +0800

    new commit

---
+++ camera.py
@@ -0,0 +1,17 @@
+import cv2
+from picamera2 import Picamera2
+import time
+
+picam2 = Picamera2()
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)})
+picam2.configure(camera_config)
+picam2.start()
+time.sleep(2)
+
+img = picam2.capture_array()
+
+cv2.imshow("output", img)
+cv2.waitKey(0)
+
+picam2.stop()
+picam2.close()

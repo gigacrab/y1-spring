1960925764c32ead7f35f6ca694cf46b7c022fa2
Jayden Tan Yu Zhe <jaytan9810@gmail.com>
2026-03-04 10:10:27 +0800

    hi bryan

--- symbol_recognition.py
+++ symbol_recognition.py
@@ -199,7 +199,7 @@ try:
         thresh_sharp = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 151, 15)
         
         # --- THE GLUE ---
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
         thresh_glued = cv2.morphologyEx(thresh_sharp, cv2.MORPH_CLOSE, kernel)
 
         best_label = None

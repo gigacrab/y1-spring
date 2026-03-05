f677db480b3debb7787c1b52f68f1a136798b2d9
Jayden Tan Yu Zhe <jaytan9810@gmail.com>
2026-03-05 11:16:30 +0800

    hi bryan

--- symbol_recognition_final.py
+++ symbol_recognition_final.py
@@ -152,7 +152,7 @@ try:
                                 # 4. The arrow points in the opposite direction of the heaviest edge!
                                 geom_match = max(masses, key=masses.get)
                         # --------------------------------
-                        if geom_match == ["Star", "Kite"]:
+                        if geom_match in ["Star", "Kite"]:
                             # Draw a rotating box that perfectly hugs the shape
                             rect = cv2.minAreaRect(c)
                             w_rect, h_rect = rect[1]

8da56d339c2dce9fc1c88b3c667a0df11fe9cf31
gigacrab <tehwenliang@gmail.com>
2026-03-05 11:05:46 +0800

    bryan

--- symbol_recognition_final.py
+++ symbol_recognition_final.py
@@ -101,7 +101,7 @@ try:
                             holes += 1
                     
                     # IF THE SHAPE IS COMPLETELY SOLID (0 HOLES), IT'S A BASIC GEOMETRY SHAPE!
-                    if holes == 0:
+                    if holes == 0 or cv2.contourArea(c) > 2500:
                         live_moments = cv2.HuMoments(cv2.moments(c)).flatten()
                         lowest_diff = 0.05 
                         geom_match = None

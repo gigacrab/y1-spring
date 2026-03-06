3c054d5b38b8f87e9439287aee342b4a6848e312
gigacrab <tehwenliang@gmail.com>
2026-03-06 12:47:21 +0800

    bryan

--- hard.py
+++ hard.py
@@ -76,7 +76,7 @@ while True:
         frame = picam2.capture_array()
 
         # line following
-        roi = frame[200:480, :]
+        roi = frame[0:240, :]
 
         #cv2.imshow("raw", im)
         imgray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
@@ -92,7 +92,7 @@ while True:
 
         # hierarchy -> [next, previous, first_child, parent]
         contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        im2 = np.zeros((280, 640, 3), dtype=np.uint8)
+        im2 = np.zeros((240, 640, 3), dtype=np.uint8)
         cv2.drawContours(im2, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
         count = 0
         if len(contours) > 0:

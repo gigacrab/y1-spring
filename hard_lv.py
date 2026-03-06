41742cd1462af795a9a5051d06fa3b075ea81812
gigacrab <tehwenliang@gmail.com>
2026-03-06 14:55:42 +0800

    bryan

--- hard_lv.py
+++ hard_lv.py
@@ -16,8 +16,7 @@ def shape_rec(frame):
     # ==========================================
     # PHASE 1: GEOMETRY FIRST
     # ==========================================
-    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 255, 8)
-    
+    thresh = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     kernel = np.ones((3, 3), np.uint8)
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
     

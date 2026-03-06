c9f7e9238f932117f3f5b4a0068cd4e9c84cbe7e
gigacrab <tehwenliang@gmail.com>
2026-03-06 13:05:19 +0800

    bryan

--- hard.py
+++ hard.py
@@ -111,7 +111,7 @@ while True:
 
             if not flag and count >= 2 and count <= 4:
                 movement.move(0, 0)
-                time.sleep(0.2)
+                time.sleep(1)
                 hello = picam2.capture_array()
                 cv2.imshow("hello", hello)
 

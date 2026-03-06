237b9683707fd601c075bd0c590da8cd73ae75fd
Jayden Tan Yu Zhe <jaytan9810@gmail.com>
2026-03-06 13:46:03 +0800

    lf_n_ssr

--- hard.py
+++ hard.py
@@ -16,7 +16,7 @@ def shape_rec(frame):
     # ==========================================
     # PHASE 1: GEOMETRY FIRST
     # ==========================================
-    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 200, 8)
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 201, 8)
 
     
     kernel = np.ones((3, 3), np.uint8)

d0c80906fa680efa0d532ec83e6f7d599872c36b
gigacrab <tehwenliang@gmail.com>
2026-03-06 10:07:58 +0800

    bryan

--- bryan/jayden.py
+++ bryan/jayden.py
@@ -171,7 +171,7 @@ try:
                         if geom_match:
                             best_match = geom_match
                             x, y, w, h = cv2.boundingRect(c)
-                            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                            cv2.rectangle(thresh, (x, y), (x+w, y+h), (0, 255, 0), 2)
                             break 
 
         # ==========================================

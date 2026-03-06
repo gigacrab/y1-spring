410bc03b01ffa82095a0e51009325f5445996bbf
gigacrab <tehwenliang@gmail.com>
2026-03-06 12:44:56 +0800

    bryan

--- hard.py
+++ hard.py
@@ -111,6 +111,7 @@ while True:
 
             if not flag and count >= 2 and count <= 4:
                 movement.move(0, 0)
+                movement.move(-0.5, -0.5)
                 time.sleep(1)
                 time_cool = time.perf_counter()
                 time_marker = time.perf_counter()

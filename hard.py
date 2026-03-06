2f46ad2fcbefd735d228913cba167da55859dad4
gigacrab <tehwenliang@gmail.com>
2026-03-06 13:08:32 +0800

    bryan

--- hard.py
+++ hard.py
@@ -110,6 +110,7 @@ while True:
             print(count)
 
             if not flag and count >= 2 and count <= 4:
+                time.sleep(0.1)
                 movement.move(0, 0)
                 time.sleep(1)
                 hello = picam2.capture_array()

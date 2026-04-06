91b52e3a894a71e184012f000dd3271cbd2c46ed
gigacrab <tehwenliang@gmail.com>
2026-04-06 20:46:31 +0800

    trying my best

--- bryan/jayden-snsr.py
+++ bryan/jayden-snsr.py
@@ -131,7 +131,7 @@ try:
             blur, 255,
             cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
             cv2.THRESH_BINARY_INV,
-            51, 8
+            51, 5
         )
 
         kernel = np.ones((3, 3), np.uint8)

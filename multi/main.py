d1240af9e28b3109f79687a160f47651f01747be
gigacrab <tehwenliang@gmail.com>
2026-04-07 20:18:06 +0800

    trying my best

--- multi/main.py
+++ multi/main.py
@@ -10,35 +10,39 @@ FRAME_SHAPE = (480, 640, 3)
 FRAME_DTYPE = np.uint8
 FRAME_NBYTES = int(np.prod(FRAME_SHAPE)) * np.dtype(FRAME_DTYPE).itemsize
 
-def camera_process(shm_name, lock, new_frame_event, stop_event):
+def camera_process(shm_name, lock, shape_event, line_event, stop_event):
+    shm = shared_memory.SharedMemory(name=shm_name)
+    frame_buf = np.ndarray(FRAME_SHAPE, dtype=FRAME_DTYPE, buffer=shm.buf)
+    
     picam2 = Picamera2()
     picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
     picam2.start()
     time.sleep(2)
+    
     try:
         while not stop_event.is_set():
             frame = picam2.capture_array()
-
-            # Doesn't block if a queue is full, drops the current frame instead
-            if not frame_q_shape.full():
-                frame_q_shape.put(frame)
-            if not frame_q_line.full():
-                frame_q_line.put(frame)
+            with lock:
+                np.copyto(frame_buf, frame)
+            shape_event.set()
+            line_event.set()
     
     finally:
         shm.close()
         picam2.stop()
         picam2.close()
 
-def shape_detection_process(shm_name, lock, new_frame_event, result_q, stop_event):
+def shape_detection_process(shm_name, lock, shape_event, result_q, stop_event):
     shm = shared_memory.SharedMemory(name=shm_name)
     frame_buf = np.ndarray(FRAME_SHAPE, dtype=FRAME_DTYPE, buffer=shm.buf)
     
     try:
         while not stop_event.is_set():
-            new_frame_event.wait()
+            shape_event.wait()
+            shape_event.clear()
             with lock:
                 frame = frame_buf.copy()
+            
             pred = object_detection.detect_object(frame)
 
             if pred is not None and not result_q.full():
@@ -47,7 +51,7 @@ def shape_detection_process(shm_name, lock, new_frame_event, result_q, stop_even
         shm.close()
         object_detection.stop()
 
-def line_following_process(shm_name, lock, new_frame_event, result_q, stop_event):
+def line_following_process(shm_name, lock, line_event, result_q, stop_event):
     shm = shared_memory.SharedMemory(name=shm_name)
     frame_buf = np.ndarray(FRAME_SHAPE, dtype=FRAME_DTYPE, buffer=shm.buf)
 
@@ -55,10 +59,11 @@ def line_following_process(shm_name, lock, new_frame_event, result_q, stop_event
         while not stop_event.is_set():
             time_marker = time.perf_counter()
             
-            new_frame_event.wait()
+            line_event.wait()
+            line_event.clear()
             with lock:
                 frame = frame_buf.copy()
-
+            
             time_marker2 = time.perf_counter()
             print(f"duration1 {time_marker2 - time_marker}")
 
@@ -87,16 +92,17 @@ if __name__ == "__main__": # what was ran with python
     result_q = mp.Queue(maxsize=5)
 
     stop_event = mp.Event()
-    new_frame_event = mp.Event()
+    shape_event = mp.Event()
+    line_event = mp.Event()
     lock = mp.Lock()
 
     processes = [
         mp.Process(target=camera_process, 
-                   args=(shm.name, lock, new_frame_event, stop_event)),
+                   args=(shm.name, lock, shape_event, stop_event)),
         mp.Process(target=shape_detection_process, 
-                   args=(shm.name, lock, new_frame_event, result_q, stop_event)),
+                   args=(shm.name, lock, shape_event, result_q, stop_event)),
         mp.Process(target=line_following_process, 
-                   args=(shm.name, lock, new_frame_event, result_q, stop_event)),
+                   args=(shm.name, lock, line_event, result_q, stop_event)),
     ]
 
     for p in processes:
@@ -108,6 +114,8 @@ if __name__ == "__main__": # what was ran with python
             p.join() # blocks here
     except KeyboardInterrupt:
         stop_event.set()
+        shape_event.set()
+        line_event.set()
         '''for p in processes:
             p.terminate()'''
     finally:

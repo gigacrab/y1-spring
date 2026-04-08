80378dddcb25bd782e5827d40a5a8ea25aed6f44
gigacrab <tehwenliang@gmail.com>
2026-04-08 12:03:12 +0800

    trying my best

--- multi/line_following.py
+++ multi/line_following.py
@@ -87,7 +87,7 @@ def follow_line(frame):
     # ── Find best black contour ───────────────────────────────────────────
     valid_black_cnt = None
     black_cx        = None
-    if ret < 180:
+    if ret < 150:
         black_cnts, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in sorted(black_cnts, key=cv2.contourArea, reverse=True):
             if 7500 <= cv2.contourArea(cnt) <= 40000:

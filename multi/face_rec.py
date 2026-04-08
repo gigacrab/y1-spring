34b27abb32e1989f0304df292dfa50aa57e9299e
gigacrab <tehwenliang@gmail.com>
2026-04-08 16:29:50 +0800

    trying my best

--- multi/face_rec.py
+++ multi/face_rec.py
@@ -60,11 +60,11 @@ while True:
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]'''
         
-        rgb_small_frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
 
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_locations = face_recognition.face_locations(rgb_frame)
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
@@ -72,7 +72,7 @@ while True:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
-            print(matches)
+            
 
             # # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
@@ -86,6 +86,7 @@ while True:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
+            print(name)
 
     process_this_frame = not process_this_frame
 
@@ -106,6 +107,7 @@ while True:
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
+    
     # Display the resulting image
     cv2.imshow('Video', frame)
 

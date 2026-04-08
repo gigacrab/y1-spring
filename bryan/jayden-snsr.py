002976207d4f6c330c2a1aa1c67ee8d0213c9671
gigacrab <tehwenliang@gmail.com>
2026-04-08 10:47:31 +0800

    trying my best

--- bryan/jayden-snsr.py
+++ bryan/jayden-snsr.py
@@ -126,10 +126,10 @@ try:
         frame = picam2.capture_array()
         output = frame.copy()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        #blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
         thresh = cv2.adaptiveThreshold(
-            blur, 255,
+            gray, 255,
             cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
             cv2.THRESH_BINARY_INV,
             51, 5

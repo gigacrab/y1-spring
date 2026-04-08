9942b39cc663ee4b6e75e610f272e1f261598586
gigacrab <tehwenliang@gmail.com>
2026-04-08 11:17:43 +0800

    trying my best

--- jayden/detect_object.py
+++ jayden/detect_object.py
@@ -123,7 +123,7 @@ def detect_object(frame):
         gray, 255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
         cv2.THRESH_BINARY_INV,
-        51, 5
+        51, 3
     )
 
     kernel = np.ones((3, 3), np.uint8)

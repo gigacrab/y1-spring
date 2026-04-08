73b95efa4edc038ee2a6f8e6e33eeab3543396f4
gigacrab <tehwenliang@gmail.com>
2026-04-08 16:21:28 +0800

    trying my best

--- multi/face_rec.py
+++ multi/face_rec.py
@@ -55,11 +55,13 @@ while True:
     # Only process every other frame of video to save time
     if process_this_frame:
         # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        '''small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        rgb_small_frame = small_frame[:, :, ::-1]'''
         
+        rgb_small_frame = frame
+
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -110,5 +112,5 @@ while True:
         break
 
 # Release handle to the webcam
-video_capture.release()
+picam2.stop()
 cv2.destroyAllWindows()

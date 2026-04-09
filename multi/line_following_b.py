5a7c136e8620e5dcd9b96374606bd3c306428f80
gigacrab <tehwenliang@gmail.com>
2026-04-09 12:33:43 +0800

    trying my best

--- multi/line_following_b.py
+++ multi/line_following_b.py
@@ -101,7 +101,7 @@ def follow_line(frame):
     # however the Otsu method wasn't that good because it'd always find a region of threshold
     # also idc about the ret
     ret, _ = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    _, thresh = cv2.threshold(imgray, ret + 40, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(imgray, ret - 20, 255, cv2.THRESH_BINARY_INV)
     #_, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY_INV)
     #cv2.imshow("thresh", thresh)
     print(f"ret {ret}")

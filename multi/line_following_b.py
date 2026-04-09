e8096b43299337bcd20075679f2d6f1af422feed
gigacrab <tehwenliang@gmail.com>
2026-04-09 13:18:44 +0800

    trying my best

--- multi/line_following_b.py
+++ multi/line_following_b.py
@@ -60,9 +60,11 @@ def turn_360():
 def shift(dir):
     if dir.lower() == "left":
         movement.move(-1, 1)
+        time.sleep(0.2)
         print("helllllllllllllo")
     elif dir.lower() == "right":
         movement.move(1, -1)
+        time.sleep(0.2)
         print("byeeeeeeeeee")
 
 def calc_pid(cx, time_marker):

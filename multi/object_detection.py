928ec6cec04fffb01c18f94fd1fcff850497b7a2
gigacrab <tehwenliang@gmail.com>
2026-04-09 14:13:36 +0800

    trying my best

--- multi/object_detection.py
+++ multi/object_detection.py
@@ -140,9 +140,9 @@ def detect_object(frame):
     answer = []
     containers = []
 
-    #cv2.imshow("frame", frame)
-    #cv2.imshow("close", closed)
-    #cv2.waitKey(1)
+    cv2.imshow("frame", frame)
+    cv2.imshow("close", closed)
+    cv2.waitKey(1)
 
     # should already have hierarchy if a contour exists
     for i, c in enumerate(cnts):

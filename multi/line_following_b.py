021a79e300ff80078e8e7eb2f04deed6f1665a08
gigacrab <tehwenliang@gmail.com>
2026-04-09 16:20:02 +0800

    trying my best

--- multi/line_following_b.py
+++ multi/line_following_b.py
@@ -118,8 +118,12 @@ def follow_line(frame):
     # 0 - values above this, assigned 255, the Otsu method adjusts according to lighting
     # however the Otsu method wasn't that good because it'd always find a region of threshold
     # also idc about the ret
-    ret, _ = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    _, thresh = cv2.threshold(imgray, ret - 20, 255, cv2.THRESH_BINARY_INV)
+    thresh = cv2.adaptiveThreshold(
+        imgray, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY_INV,
+        51, 8
+    )
     #_, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY_INV) 
 
     #cv2.imshow("thresh", thresh)

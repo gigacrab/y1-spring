078a230e2ac707e80b79a3f3a4df8ce6f1536015
gigacrab <tehwenliang@gmail.com>
2026-04-10 12:04:38 +0800

    trying my best

--- multi/line_following_b.py
+++ multi/line_following_b.py
@@ -154,7 +154,7 @@ def follow_line(frame):
     black_sorted = sorted(black_cnts, key=cv2.contourArea, reverse=True)
     if black_sorted and cv2.contourArea(black_sorted[0]) > 7500:
         black_target = black_sorted[0]
-        #print(f"black area {cv2.contourArea(black_target)}")
+        print(f"black area {cv2.contourArea(black_target)}")
         cv2.drawContours(thresh, [black_target], -1, (0, 255, 0), thickness=cv2.FILLED)
         M = cv2.moments(black_target)
         if M['m00'] != 0:
@@ -163,7 +163,7 @@ def follow_line(frame):
     color_sorted = sorted(color_cnts, key=cv2.contourArea, reverse=True)
     if color_sorted and cv2.contourArea(color_sorted[0]) > 2000:
         color_target = color_sorted[0]
-        #print(f"color area {cv2.contourArea(color_target)}")
+        print(f"color area {cv2.contourArea(color_target)}")
         cv2.drawContours(thresh, [color_target], -1, (0, 0, 255), thickness=cv2.FILLED)
         M = cv2.moments(color_target)
         if M['m00'] != 0:

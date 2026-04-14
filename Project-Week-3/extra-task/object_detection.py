cf095e381863bcaca6b44656b758c7f0f5dc724a
gigacrab <tehwenliang@gmail.com>
2026-04-14 23:09:19 +0800

    Renamed find container function

--- Project-Week-3/extra-task/object_detection.py
+++ Project-Week-3/extra-task/object_detection.py
@@ -54,7 +54,7 @@ def detect_symbols_in_container(i, cnts, hrchy):
         return "QR Code"
     return "No idea"
 
-def detect_container(i, c, cnts, hrchy):
+def find_valid_container(i, c, cnts, hrchy):
     area = cv2.contourArea(c)
     if area < MIN_AREA:
         return None
@@ -147,7 +147,7 @@ def detect_object(frame):
     # should already have hierarchy if a contour exists
     for i, c in enumerate(cnts):
         pred = "No idea"
-        result = detect_container(i, c, cnts, hrchy)
+        result = find_valid_container(i, c, cnts, hrchy)
 
         if result is not None: # found container
             if isinstance(result, (int, np.integer)): # no valid contours
